13ad1c412cd4dee9eef4617110705456244a233c
TrinhDangDang <73269352+TrinhDangDang@users.noreply.github.com>
2024-11-07 14:24:05 -0600

    adjust the base url to baseUrl: 'https://api.trinhdangdang.com',

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { setCredentials } from '../../features/auth/authSlice'
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'http://localhost:3500',
+    baseUrl: 'https://api.trinhdangdang.com',
     credentials: 'include',             //ensures that cookies are included in requests
     prepareHeaders: (headers, { getState }) => {
         const token = getState().auth.token
