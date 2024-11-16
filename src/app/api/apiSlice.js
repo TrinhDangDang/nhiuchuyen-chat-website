4d9d76404dde2e689550cac513993b592c695280
TrinhDangDang <73269352+TrinhDangDang@users.noreply.github.com>
2024-11-16 13:51:57 -0600

    bring the google font link from css to index.html

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { setCredentials } from '../../features/auth/authSlice'
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'https://api.trinhdangdang.com',
+    baseUrl: 'http://localhost:3500',
     credentials: 'include',             //ensures that cookies are included in requests
     prepareHeaders: (headers, { getState }) => {
         const token = getState().auth.token
